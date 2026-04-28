76323e284299af255e8deb73c837e611ba5eeb03
Peter Haller <peter.haller@prioritize-iot.de>
2026-04-28 22:26:19 +0200

    unused imports

--- src/main/java/de/hallerweb/enterprise/prioritize/PrioritizeApplication.java
+++ src/main/java/de/hallerweb/enterprise/prioritize/PrioritizeApplication.java
@@ -2,7 +2,6 @@ package de.hallerweb.enterprise.prioritize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
